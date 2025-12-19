fd12110491e7271f21bcef6d702914d33318bf01
ims <elementium7@gmail.com>
2025-12-20 05:00:27 +1300

    EAStorageMeshShape to obj 2

--- simpsons_game/EAStorageMeshShape/main.go
+++ simpsons_game/EAStorageMeshShape/main.go
@@ -5,157 +5,191 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"strings"
+	"unsafe"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		return
+	if len(os.Args) < 1 {
+		panic("")
 	}
 
-	path := os.Args[1]
-	// path := "/tmp/collisionmodel1.hkt.PS3"
-	outdir := path[:len(path)-8]
+	pathIn := os.Args[1]
 
-	f, err := os.Open(path)
+	f, err := os.Open(pathIn)
 	if err != nil {
-		panic(err)
+		panic("")
 	}
 	defer f.Close()
 
-	cno := peeku(f, 0x64) + 0x10
-	ea := findEA(f, int64(cno))
+	///// //// //
+	///  parse pointers
 
-	dso := peeku(f, 0x94) + 0x10
-	cmo := peeku(f, 0xa0) + dso
-	eof := peeku(f, 0xa4) + dso
+	var hkxSize uint32 = 0x10
 
-	f.Seek(int64(cmo), io.SeekStart)
+	var classNamesOffset uint32
+	f.Seek(0x64, io.SeekStart)
+	binary.Read(f, binary.BigEndian, &classNamesOffset)
+	classNamesOffset += hkxSize
 
-	var cmt struct {
-		O  uint32
-		F  uint32
-		So uint32
+	// var eaStringOffset uint32
+	// f.Seek()
+
+	var dataSectionOffset uint32
+	f.Seek(0x94, io.SeekStart)
+	binary.Read(f, binary.BigEndian, &dataSectionOffset)
+	dataSectionOffset += hkxSize
+
+	var classMappingOffset uint32
+	f.Seek(0xa0, io.SeekStart)
+	binary.Read(f, binary.BigEndian, &classMappingOffset)
+	classMappingOffset += dataSectionOffset
+
+	var endOfFile uint32
+	binary.Read(f, binary.BigEndian, &endOfFile)
+	endOfFile += dataSectionOffset
+
+	///// //// //
+	///  parse classnames
+
+	var nameMeta struct {
+		Hash  uint32
+		Flags uint8
+	}
+	classNames := make(map[uint32]string)
+
+	f.Seek(int64(classNamesOffset), io.SeekStart)
+
+	for {
+		binary.Read(f, binary.BigEndian, &nameMeta)
+		if nameMeta.Flags != 0x9 {
+			break
+		}
+
+		nameOffset, _ := f.Seek(0, io.SeekCurrent)
+		nameOffset -= int64(classNamesOffset)
+
+		name := readCstr(f)
+		classNames[uint32(nameOffset)] = name
 	}
 
-	fmt.Printf("eof: %x", eof)
-	var eas []int64
+	///// //// //
+	///  parse classmappings
+
+	type ClassMapping struct {
+		DataOffset   uint32
+		Flags        uint32
+		StringOffset uint32
+	}
+	var classMapping ClassMapping
+	var classMappingArray []ClassMapping
+
+	_, err = f.Seek(int64(classMappingOffset), io.SeekStart)
+	if err != nil {
+		panic("")
+	}
+
 	for {
+		cursor, _ := f.Seek(0, io.SeekCurrent)
+		if endOfFile < uint32(cursor)+uint32(unsafe.Sizeof(classMapping)) {
+			break
+		}
+
+		binary.Read(f, binary.BigEndian, &classMapping)
+		if int32(classMapping.DataOffset) == -1 {
+			break
+		}
+
+		classMappingArray = append(classMappingArray, classMapping)
+	}
+
+	///// //// //
+	///
+
+	var eaStorageMeshShapeOffset uint32
+	for o, n := range classNames {
+		if n == "EAStorageMeshShape" {
+			eaStorageMeshShapeOffset = o
+			break
+		}
+	}
+
+	var eaStorageMeshShapePointers []uint32
+	for _, cm := range classMappingArray {
+		if cm.StringOffset == eaStorageMeshShapeOffset {
+			fmt.Println("Found EAStorageMeshShape at index:", cm.DataOffset)
+			eaStorageMeshShapePointers = append(eaStorageMeshShapePointers, cm.DataOffset+dataSectionOffset)
+		}
+	}
+
+	///// //// //
+	///  parse EAStorageMeshShape instances
+
+	type Vertex = [4]float32
+	type Face = [3]uint32
+
+	type EAStorageMeshShapeInstance struct {
+		Vertices []Vertex
+		Faces    []Face
+	}
+	var eaStorageMeshShapeInstances []EAStorageMeshShapeInstance
+	var vertexCount uint32
+	var faceCount uint32
+	var vertex Vertex
+	var face Face
+
+	for _, ptr := range eaStorageMeshShapePointers {
+		var vertices []Vertex
+		var faces []Face
+
+		f.Seek(int64(ptr), io.SeekStart)
+		f.Seek(0x18, io.SeekCurrent) // skip
+		binary.Read(f, binary.BigEndian, &vertexCount)
+
+		f.Seek(8, io.SeekCurrent)
+		binary.Read(f, binary.BigEndian, &faceCount)
+
+		f.Seek(0xa8, io.SeekCurrent)
 		pos, _ := f.Seek(0, io.SeekCurrent)
-		fmt.Printf("pos: %x\n", pos)
-
-		if pos+0xc > int64(eof) {
-			break
+		fmt.Println("pos:", pos)
+		for i := 0; i < int(vertexCount); i++ {
+			binary.Read(f, binary.BigEndian, &vertex)
+			vertices = append(vertices, vertex)
 		}
-
-		// f.Seek(0x293c4, io.SeekStart)
-		binary.Read(f, binary.BigEndian, &cmt)
-		if int32(cmt.O) == -1 {
-			break
+		for i := 0; i < int(faceCount); i++ {
+			binary.Read(f, binary.BigEndian, &face)
+			faces = append(faces, face)
 		}
-		if cmt.So == ea {
-			fmt.Println(cmt.So, ea)
-			eas = append(eas, int64(cmt.O+dso))
-		}
+		eaStorageMeshShapeInstances = append(eaStorageMeshShapeInstances, EAStorageMeshShapeInstance{
+			Vertices: vertices,
+			Faces:    faces,
+		})
 	}
-	fmt.Println(eas)
-	for _, nea := range eas {
-		fmt.Printf("nea: %x\n", nea)
-		n, obj := ea2obj(f, int64(nea))
-		err := os.MkdirAll(outdir, 0755)
-		if err != nil {
-			fmt.Println("Error creating directory:", err)
-			return
+
+	var vCount uint32 = 1
+	vString := ""
+	fString := ""
+	for _, inst := range eaStorageMeshShapeInstances {
+		for _, v := range inst.Vertices {
+			vString += fmt.Sprintf("v %.6f %.6f %.6f\n", v[0], v[1], v[2])
 		}
-		p := fmt.Sprintf("%s/%s.obj", outdir, n)
-
-		of, err := os.Create(p)
-		if err != nil {
-			panic(err)
+		for _, f := range inst.Faces {
+			fString += fmt.Sprintf("f %d %d %d\n", f[0]+vCount, f[1]+vCount, f[2]+vCount)
 		}
-		defer of.Close()
-
-		_, err = of.WriteString(obj)
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println(obj)
-
+		vCount += uint32(len(inst.Vertices))
 	}
+
+	obj, err := os.Create(os.Args[1] + ".obj")
+	if err != nil {
+		panic(err)
+	}
+
+	defer obj.Close()
+
+	obj.WriteString(vString)
+	obj.WriteString(fString)
 }
 
-func ea2obj(f *os.File, o int64) (string, string) {
-	var s strings.Builder
-
-	np := o - 0x80
-	f.Seek(np, io.SeekStart)
-	n := cstr(f)
-	n = strings.ReplaceAll(n, "|", "_")
-
-	var vn uint32
-	f.Seek(o, io.SeekStart)
-	f.Seek(0x18, io.SeekCurrent)
-	binary.Read(f, binary.BigEndian, &vn)
-
-	var fn uint32
-	f.Seek(0x8, io.SeekCurrent)
-	binary.Read(f, binary.BigEndian, &fn)
-
-	f.Seek(0xa8, io.SeekCurrent)
-	var v struct {
-		X float32
-		Y float32
-		Z float32
-		C float32
-	}
-	for i := 0; i < int(vn); i++ {
-		binary.Read(f, binary.BigEndian, &v)
-		fmt.Fprintf(&s, "v %.6f %.6f %.6f\n", v.X, v.Y, v.Z)
-	}
-
-	s.WriteString("\n")
-
-	var ff struct {
-		A uint32
-		B uint32
-		C uint32
-	}
-	for i := 0; i < int(fn); i++ {
-		binary.Read(f, binary.BigEndian, &ff)
-		fmt.Fprintf(&s, "f %d %d %d\n", ff.A+1, ff.B+1, ff.C+1)
-	}
-
-	return n, s.String()
-}
-
-func findEA(f *os.File, o int64) uint32 {
-	f.Seek(o, io.SeekStart)
-	for {
-		var flag int8
-		f.Seek(4, io.SeekCurrent)
-		binary.Read(f, binary.LittleEndian, &flag)
-		if flag != 9 {
-			break
-		}
-		p, _ := f.Seek(0, io.SeekCurrent)
-		s := cstr(f)
-
-		if s == "EAStorageMeshShape" {
-			return uint32(p - o)
-		}
-	}
-	panic("didn't find EAStoreageMeshShape string")
-}
-
-func peeku(f *os.File, o int64) uint32 {
-	f.Seek(o, io.SeekStart)
-
-	var u uint32
-	binary.Read(f, binary.BigEndian, &u)
-	return u
-}
-func cstr(f *os.File) string {
+func readCstr(f *os.File) string {
 	var buf []byte
 	b := make([]byte, 1)
 
@@ -172,9 +206,4 @@ func cstr(f *os.File) string {
 	}
 
 	return string(buf)
-}
-
-func pp(f *os.File, s string) {
-	c, _ := f.Seek(0, io.SeekCurrent)
-	fmt.Printf("%s: %x\n", s, c)
 }
